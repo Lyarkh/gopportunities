b03a794b5fb7d9ac0012e12865f30728810d1532
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-10-09 22:12:28 -0300

    refactor: adicionando chamada package router na main

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "github.com/Lyarkh/gopportunities/router"
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run(":8080")
+	router.Initialize()
 }

74f725ad4afa6df521e34639bead7ebb4150360c
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-10-09 21:36:02 -0300

    refactor: modificando nome da variável router

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	router.Run(":8080")
 }

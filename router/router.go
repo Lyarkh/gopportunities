192bdc65c0dc9b781d57f40b210f89658802fc52
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-10-27 23:02:53 -0300

    feat: criando routes, para separação dos routes da aplicação

--- router/router.go
+++ router/router.go
@@ -4,10 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func Initialize() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	
 	router.Run(":8080")
 }

16522c65687d0a7af8cc0d9dfba9694e5b834f39
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-11-12 12:38:19 -0300

    feat: criando base de mais handlers para a api

--- handler/handler.go
+++ handler/handler.go
@@ -11,3 +11,23 @@ func CreateOpeningHandler(ctx *gin.Context) {
 		"message": "GET opening",
 	})
 }
+func ShowOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET opening",
+	})
+}
+func DeleteOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET opening",
+	})
+}
+func UpdateOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET opening",
+	})
+}
+func ListOpeningsHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET opening",
+	})
+}

61874db2856758a381b3edd33fc3975ec4ae0c52
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-12-04 21:48:13 -0300

    feat: base logger do package config

---
+++ config/logger.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type Logger struct {
+	debug   *log.Logger
+	info    *log.Logger
+	warning *log.Logger
+	err     *log.Logger
+	writer  io.Writer
+}
+
+func NewLogger(p string) *Logger {
+	writer := io.Writer(os.Stdout)
+	logger := log.New(writer, p, log.Ldate|log.Ltime)
+
+	return &Logger{
+		debug: log.New(writer, "DEBUG: ", logger.Flags() ),
+		info: log.New(writer, "INFO: ", logger.Flags() ),
+		warning: log.New(writer, "WARNING: ", logger.Flags() ),
+		err: log.New(writer, "ERROR: ", logger.Flags() ),
+		writer: writer,
+	}
+}

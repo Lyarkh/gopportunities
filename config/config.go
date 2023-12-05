8496facc0fa124d6662b7f58355077ea0c4b16f8
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-12-04 21:58:32 -0300

    feat: criando GetLogger no config.go

--- config/config.go
+++ config/config.go
@@ -3,9 +3,16 @@ package config
 import "gorm.io/gorm"
 
 var (
-	db *gorm.DB
+	db     *gorm.DB
+	logger *Logger
 )
 
 func Init() error {
 	return nil
+}
+
+
+func GetLogger(p string) *Logger {
+	logger = NewLogger(p)
+	return logger
 }

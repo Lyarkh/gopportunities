d9c447b8792872d54a8e97b5f451e01dc2e884c0
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-12-04 21:55:50 -0300

    feat: func para struct logger dos logs com formatação

--- config/logger.go
+++ config/logger.go
@@ -42,3 +42,19 @@ func (l *Logger) Warning(v ...interface{}) {
 func (l *Logger) Error(v ...interface{}) {
 	l.err.Println(v...)
 }
+
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	l.debug.Printf(format, v...)
+}
+
+func (l *Logger) Infof(format string, v ...interface{}) {
+	l.info.Printf(format, v...)
+}
+
+func (l *Logger) Warningf(format string, v ...interface{}) {
+	l.warning.Printf(format, v...)
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	l.err.Printf(format, v...)
+}

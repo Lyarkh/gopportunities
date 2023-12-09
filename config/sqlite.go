674cdd30071253df57697ef2d3af3b16a93a348d
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-12-09 20:37:05 -0300

    feat: validação para caso o database não exista

--- config/sqlite.go
+++ config/sqlite.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"os"
+
 	"github.com/Lyarkh/gopportunities/schemas"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -8,7 +10,27 @@ import (
 
 func InitializeSQLite() (*gorm.DB, error) {
 	logger := GetLogger("sqlite")
-	db, err := gorm.Open(sqlite.Open("./db/main.db"), &gorm.Config{})
+
+	dbPath := "./db/main.db"
+	_, err := os.Stat(dbPath)
+
+	if os.IsNotExist(err) {
+		logger.Info("database file not found, creating...")
+
+		err = os.MkdirAll("./db", os.ModePerm)
+
+		if err != nil {
+			return nil, err
+		}
+
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 
 	if err != nil {
 		logger.Errorf("sqlite opening error: %v", err)

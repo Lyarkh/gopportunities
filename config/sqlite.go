fd9785976ec2c1990df60dce9d316492c4d6560f
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-12-09 20:31:45 -0300

    feat: base init database

--- config/sqlite.go
+++ config/sqlite.go
@@ -1,11 +1,19 @@
 package config
 
 import (
+	"github.com/Lyarkh/gopportunities/schemas"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func InitializeSQLite() (*gorm.DB, error) {
 	logger := GetLogger("sqlite")
-	gorm.Open(sqlite.Open())
+	db, err := gorm.Open(sqlite.Open("./db/main.db"), &gorm.Config{})
+
+	if err != nil {
+		logger.Errorf("sqlite opening error: %v", err)
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&schemas.Opening{})
 }

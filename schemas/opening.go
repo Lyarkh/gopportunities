7cb987c33789d82a95a3139f38edbf28045a2d4e
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-12-10 23:10:00 -0300

    feat: adicionando referencia de objeto json do struct

--- schemas/opening.go
+++ schemas/opening.go
@@ -17,13 +17,14 @@ type Opening struct {
 }
 
 type OpeningResponse struct {
-	ID        uint
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Role      string
-	Company   string
-	Location  string
-	Remote    bool
-	Link      string
-	Salary    int64
+	ID        uint      `json: "id"`
+	CreatedAt time.Time `json: "createdAt"`
+	UpdatedAt time.Time `json: "updatedAt"`
+	DeletedAt time.Time `json: "deletedAt"`
+	Role      string    `json: "role"`
+	Company   string    `json: "company"`
+	Location  string    `json: "location"`
+	Remote    bool      `json: "remote"`
+	Link      string    `json: "link"`
+	Salary    int64     `json: "salary"`
 }

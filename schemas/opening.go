ebe20740144b395d48cd6e09c15bbd5e142937f2
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-12-12 20:35:59 -0300

    style: atualizado tags json struct

--- schemas/opening.go
+++ schemas/opening.go
@@ -17,14 +17,14 @@ type Opening struct {
 }
 
 type OpeningResponse struct {
-	ID        uint      `json: "id"`
-	CreatedAt time.Time `json: "createdAt"`
-	UpdatedAt time.Time `json: "updatedAt"`
-	DeletedAt time.Time `json: "deletedAt,omitempty"`
-	Role      string    `json: "role"`
-	Company   string    `json: "company"`
-	Location  string    `json: "location"`
-	Remote    bool      `json: "remote"`
-	Link      string    `json: "link"`
-	Salary    int64     `json: "salary"`
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
 }

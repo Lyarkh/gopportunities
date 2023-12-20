c9a8c28267909f85dc195f7c0a46a82697b8b669
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-12-19 22:55:54 -0300

    feat: base handler createOpening

--- handler/createOpeningHandler.go
+++ handler/createOpeningHandler.go
@@ -1,13 +1,12 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opening",
-	})
+	request := struct {
+		role string
+	}{}
+
 }

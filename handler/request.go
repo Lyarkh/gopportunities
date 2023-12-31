a5214c8b300ffb88b911bfac0ee32ceb85b155dc
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-12-31 12:42:55 -0300

    feat: request createopening

--- handler/request.go
+++ handler/request.go
@@ -2,5 +2,10 @@ package handler
 
 // CreateOpening
 type CreateOpeningRequest struct {
-	Role string `json:"role"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
 }

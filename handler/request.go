f72fdf7ff62f88ea02afcddfa75820e541177878
Lyarkh <lcemanuel.emanuel@gmail.com>
2024-01-01 21:00:01 -0300

    eat: atualizando tipos request

--- handler/request.go
+++ handler/request.go
@@ -5,7 +5,7 @@ type CreateOpeningRequest struct {
 	Role      string    `json:"role"`
 	Company   string    `json:"company"`
 	Location  string    `json:"location"`
-	Remote    bool      `json:"remote"`
+	Remote    *bool      `json:"remote"`
 	Link      string    `json:"link"`
 	Salary    int64     `json:"salary"`
 }

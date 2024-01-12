76b13ecbcd7f321f454832ea814601c4174a5eb7
Lyarkh <lcemanuel.emanuel@gmail.com>
2024-01-11 22:02:18 -0300

    feat: criando base opening object

--- handler/createOpeningHandler.go
+++ handler/createOpeningHandler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Lyarkh/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,8 +19,13 @@ func CreateOpeningHandler(ctx *gin.Context) {
 		return
 	}
 
+	opening := schemas.Opening{
+		
+	}
+
 	if err := db.Create(&request).Error; err != nil {
 		logger.Errorf("error creating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "erorr creating oppening on database")
 		return
 	}
 

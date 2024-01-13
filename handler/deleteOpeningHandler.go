f1906a43cdf62c7d52118f81d5b7f75e127815dc
Lyarkh <lcemanuel.emanuel@gmail.com>
2024-01-12 22:24:27 -0300

    feat: buscando opening para deleteOpening

--- handler/deleteOpeningHandler.go
+++ handler/deleteOpeningHandler.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Lyarkh/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +12,12 @@ func DeleteOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 	if id == "" {
 		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
 		return
 	}
 }

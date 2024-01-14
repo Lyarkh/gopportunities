1f7c48aa6f1e7f66f8e92a1848397fca690c9b98
Lyarkh <lcemanuel.emanuel@gmail.com>
2024-01-13 21:00:37 -0300

    feat: criiando busca de todas as openings no handler ListOpeningHandler

--- handler/listOpeningsHandler.go
+++ handler/listOpeningsHandler.go
@@ -3,11 +3,14 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Lyarkh/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opening",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+	}
 }
